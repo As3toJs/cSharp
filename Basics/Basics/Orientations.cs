d5429dc5a7f8a1caa1ce3e78a8ce48819a64de26
piyush <piyush@edulab.dk>
2018-10-25 08:26:01 +0200

    basic control flow of cSharp

---
+++ Basics/Basics/Orientations.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace Basics
+{
+    class Orientations
+    {
+        public static void Compare(int height, int width)
+        {
+            if (height == width)
+            {
+                Console.WriteLine("Image is rectangle");
+            }
+            else if (height > width)
+            {
+                Console.WriteLine("Image is Portrait");
+            }
+            else
+            {
+                Console.WriteLine("Image is Landscape");
+            }
+            Console.ReadLine();
+        }
+    }
+}
